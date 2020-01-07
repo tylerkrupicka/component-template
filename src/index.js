a9a328b83a4f3d478f39f1d60a05d9a78d0da6e4
Andrew Lisowski <lisowski54@gmail.com>
2020-01-07 14:40:33 -0800

    init js template
    add prop types to js template component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import styles from './{{pascal}}.css';
 
-interface {{pascal}}Props {
-}
-
 /** A simple component. */
-export const {{pascal}} = (props: {{pascal}}Props) => {
+export const {{pascal}} = props => {
   return <div className={styles.{{camel}}}>This is a {{pascal}} component</div>;
 }
+
+{{pascal}}.propTypes = {}
 
 export default {{pascal}};
