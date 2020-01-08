a413afc983d4b26716df3397cdfd7ce0a2818b7d
Andrew Lisowski <lisowski54@gmail.com>
2020-01-08 10:42:24 -0800

    cherry picked

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import styled from 'styled-components';
 const Wrapper = styled.div``;
 
 interface {{pascal}}Props {
+  /** The value for the component to display */
+  value: string;
 }
 
 /** A simple component. */
 export const {{pascal}} = (props: {{pascal}}Props) => {
-  return <Wrapper>This is a {{pascal}} component</Wrapper>;
+  return <Wrapper>This is a {{pascal}} component {props.value}</Wrapper>;
 }
 
 export default {{pascal}};
