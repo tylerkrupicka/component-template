d13fff96ccd71cf873227eeef183d6dab0754eb4
Andrew Lisowski <lisowski54@gmail.com>
2020-01-08 10:42:24 -0800

    make component pass lint when initted

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from './{{pascal}}.css';
 
 interface {{pascal}}Props {
+  /** The value for the component to display */
+  value: string;
 }
 
 /** A simple component. */
 export const {{pascal}} = (props: {{pascal}}Props) => {
-  return <div className={styles.{{camel}}}>This is a {{pascal}} component</div>;
+  return <div className={styles.{{camel}}}>This is a {{pascal}} component {props.value}</div>;
 }
 
 export default {{pascal}};
